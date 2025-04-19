cb01051e1a0d54f5f23513d50a3101ca1e71ca38
kuldeep-rathor <kuldeeprathor91010@gmail.com>
2025-04-19 10:39:49 +0530

    feat: add routing and new pages for the application
    This commit introduces react-router-dom for client-side routing and adds new pages (Home, About, SignIn, SignUp, Profile) to the application. The App component has been updated to include routing logic, enabling navigation between these pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
-  return <div className="bg-red-400">HEllo world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
