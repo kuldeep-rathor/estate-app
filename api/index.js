dafdea7daf84236bd8955456df2298311872b65c
Kuldeep-Ifocus <kuldeep.m@ifocussystec.in>
2025-07-12 15:16:49 +0530

    Create user update API

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ mongoose
 const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log(`Server running on port 3000!`);
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,9 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, updateUser } from "../controllers/user.controller.js";
+import { verifyTOken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 router.get("/test", test);
+router.post("/update/:id", verifyTOken, updateUser);
+
 export default router;
